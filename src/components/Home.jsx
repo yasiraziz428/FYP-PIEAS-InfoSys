1eb24746f50b3edf355fd49e80ad4bae1a4bed9d
Yasir <yasiraziz428@gmail.com>
2022-08-26 02:05:26 +0500

    Final(7)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,10 +2,25 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="container align-middle">
-      <h1 className="d-flex justify-content-center mt-5 pt-5 fst-italic opacity-75">
-        Welcome!
-      </h1>
+    <div className="container align-middle text-center">
+      <h3 className="d-flex justify-content-center pt-2 mt-5">
+        Welcome to PIEAS Information System!
+      </h3>
+      <hr></hr>
+      <div
+        className="w-50 text-center mt-5"
+        style={{
+          margin: "auto",
+          width: "60%",
+          border: "5px solid #ecf0f1",
+          padding: "10px",
+        }}
+      >
+        <p className="px-5 py-3">
+          The objective of this project is to develop an information system
+          through which we can manage workload and payment of employees
+        </p>
+      </div>
     </div>
   );
 };
